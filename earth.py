84be71a3bce50b3bd316364583c42e63f4223dd1
Noonuu <astrophysicsandpython@gmail.com>
2022-05-23 17:13:36 +0500

    [MAJOR][#2]
    * refactored functions.py
    * removed multiple instances of parameter functions into a single property_value function to reduce code length
    * added dictionary class to store the radio button dictionaries
    * refactored earth.py by removing excess if-else statements

--- earth.py
+++ earth.py
@@ -14,25 +14,16 @@ class Earth:
         self.surface_area = 510072000 * u.km**2
         self.surface_gravity = 9.80665 * u.m * u.s**-2
 
-    def convert_mass(self, into):
-        return self.mass.to('Msun') if into == 'Msun' else self.mass
+    def convert_mass(self, change_to):
+        return self.mass.to(change_to)
 
-    def convert_radius(self, into):
-        if into == 'km':
-            out = self.radius.to('km')
-        elif into == 'Rsun':
-            out = self.radius.to('Rsun')
-        else:
-            out = self.radius
+    def convert_radius(self, change_to):
+        return self.radius.to(change_to)
 
-        return out
+    def convert_surface_area(self, change_to):
+        return self.surface_area.to(change_to)
 
-    def convert_surface_area(self, into):
-        if into == 'm2':
-            out = self.surface_area.to('m**2')
-        elif into == 'Rsun2':
-            out = self.surface_area.to('Rsun**2')
-        else:
-            out = self.surface_area
-
-        return out
+    def __mass_radio_dict(self):
+        mass_radio_dict = {'g': ['g', 2],
+                           'kg': ['kg', 3],
+                           'Msun': ['Msun', 4]}
--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ import tkinter as tk
 
 from earth import Earth
 
-earth_ = Earth()
+_earth = Earth()
 
 
 def show_earth(window):
@@ -16,9 +16,34 @@ def show_earth(window):
     _f_w, _f_h = window.winfo_width(), window.winfo_height()
     earth_window.geometry(f'{_f_w}x{_f_h}')
 
-    mass_value(earth_window)
-    radius_value(earth_window)
-    surface_area_value(earth_window)
+    _dict = Dictionaries()
+
+    property_value(window=earth_window, text='Mass of Earth', row=0, value=_earth.mass,
+                   radio_dict=_dict.mass, function=_earth.convert_mass)
+
+    property_value(window=earth_window, text='Radius of Earth', row=1,
+                   value=_earth.radius, radio_dict=_dict.radius,
+                   function=_earth.convert_radius)
+
+    property_value(window=earth_window, text='Surface area of Earth', row=2,
+                   value=_earth.surface_area, radio_dict=_dict.surface_area,
+                   function=_earth.convert_surface_area)
+
+
+class Dictionaries:
+    mass = {'g': ['g', 2],
+            'kg': ['kg', 3],
+            'Msun': ['Msun', 4]}
+
+    radius = {'cm': ['cm', 2],
+              'm': ['m', 3],
+              'km': ['km', 4],
+              'Rsun': ['Rsun', 5]}
+
+    surface_area = {'cm^2': ['cm2', 2],
+                    'm^2': ['m2', 3],
+                    'km^2': ['km2', 4],
+                    'Rsun^2': ['Rsun2', 5]}
 
 
 def align_radio_buttons(window, string_var, radio_button_dict, function, row):
@@ -46,94 +71,22 @@ def entry_placement(window, value, row):
     return entry_widget
 
 
-def surface_area_value(earth_window):
+def alter_value(entry, function, change_to):
+    entry.delete(0, 'end')
+    entry.insert(0, function(change_to=change_to))
 
-    def change_surface_area_value():
-        surface_area_entry.delete(0, 'end')
-        if get_value.get() == 'km2':
-            surface_area_entry.insert(0, earth_.surface_area)
-        elif get_value.get() == 'm2':
-            surface_area_entry.insert(0, earth_.convert_surface_area('m2'))
-        elif get_value.get() == 'Rsun2':
-            surface_area_entry.insert(0, earth_.convert_surface_area('Rsun2'))
 
-    label_placement(window=earth_window,
-                    text='Surface area of Earth',
-                    row=3)
+def property_value(window, text, row, value, radio_dict, function):
+    label_placement(window=window, text=text, row=row)
 
-    surface_area_entry = entry_placement(window=earth_window,
-                                         value=earth_.surface_area,
-                                         row=3)
+    entry = entry_placement(window=window, value=value, row=row)
 
     get_value = tk.StringVar()
 
-    surface_area_radio_button = {'m^2': ['m2', 2],
-                                 'km^2': ['km2', 3],
-                                 'Rsun^2': ['Rsun2', 4]}
-
-    align_radio_buttons(window=earth_window,
+    align_radio_buttons(window=window,
                         string_var=get_value,
-                        radio_button_dict=surface_area_radio_button,
-                        function=change_surface_area_value,
-                        row=3)
-
-
-def radius_value(earth_window):
-
-    def change_radius_value():
-        radius_entry.delete(0, 'end')
-        if get_value.get() == 'm':
-            radius_entry.insert(0, earth_.radius)
-        elif get_value.get() == 'km':
-            radius_entry.insert(0, earth_.convert_radius('km'))
-        elif get_value.get() == 'Rsun':
-            radius_entry.insert(0, earth_.convert_radius('Rsun'))
-
-    label_placement(window=earth_window,
-                    text='Radius of Earth',
-                    row=1)
-
-    radius_entry = entry_placement(window=earth_window,
-                                   value=earth_.radius,
-                                   row=1)
-
-    get_value = tk.StringVar()
-
-    radius_radio_button = {'m': ['m', 2],
-                           'km': ['km', 3],
-                           'Rsun': ['Rsun', 4]}
-
-    align_radio_buttons(window=earth_window,
-                        string_var=get_value,
-                        radio_button_dict=radius_radio_button,
-                        function=change_radius_value,
-                        row=1)
-
-
-def mass_value(earth_window):
-
-    def change_mass_value():
-        mass_entry.delete(0, 'end')
-        if get_value.get() == 'kg':
-            mass_entry.insert(0, earth_.mass)
-        elif get_value.get() == 'Msun':
-            mass_entry.insert(0, earth_.convert_mass('Msun'))
-
-    label_placement(window=earth_window,
-                    text='Mass of Earth',
-                    row=0)
-
-    mass_entry = entry_placement(window=earth_window,
-                                 value=earth_.mass,
-                                 row=0)
-
-    get_value = tk.StringVar()
-
-    mass_radio_button = {'kg': ['kg', 2],
-                         'Msun': ['Msun', 3]}
-
-    align_radio_buttons(window=earth_window,
-                        string_var=get_value,
-                        radio_button_dict=mass_radio_button,
-                        function=change_mass_value,
-                        row=0)
+                        radio_button_dict=radio_dict,
+                        function=lambda: alter_value(entry=entry,
+                                                     function=function,
+                                                     change_to=get_value.get()),
+                        row=row)
