571dba7b31d25b08843c26e352b784a8ff295d61
Noonuu <astrophysicsandpython@gmail.com>
2022-06-05 18:56:25 +0500

    [MAJOR-12]
    * added frames in the application
    * added python_deployment_branch.yml to test the deployment only in branch commits
    * reworked the solar_gui.py file in class instead of function
    * updated the gui_script path in setup.py
    * removed Moon and Sun from orbital equivalencies
    * added planetary orbital parameters
    * changed the structure of planet panel
    * added docstrings, removed some functions
    * updates app version number in setup.cfg and setup.py files
    * added some more TODO lists

--- src/SolarGUI/solar_gui.py
+++ src/SolarGUI/solar_gui.py
@@ -20,128 +20,113 @@ except ImportError:
 
 # TODO: Get a good font
 # TODO: adjust/create designs in tkinter windows
-# TODO: make a planets vs Earth/Jupiter/Sun comparison dropdown menu as well
 # TODO: Segregate moons, dwarf planets, planets and other type of celestial objects.
 # TODO: Design adjustments of the GUI
-# TODO: Separate button/menu for physical/orbital and other parameters.
 # TODO: Some interesting plots (optional).
 # TODO: Random facts button.
 # TODO: Add citations
 # TODO: Add a section of papers that study different properties of the celestial objects.
+# TODO: Better GUI layouts, with some fancy stuff
+# TODO: Add more parameters
+# TODO: Add more celestial bodies
+# TODO: See that no button opens more than one window
 
-def main():
-    """
-    The main function.
 
+class Main:
 
-    Returns
-    -------
-    None.
+    def __init__(self):
+        self.width = 1024
+        self.height = 512
+        self.root_window = tk.Tk()
 
-    """
+        self.root_window.geometry(f'{self.width}x{self.height}')
+        self.root_window.minsize(self.width, self.height)
 
-    ###################################################################################
-    # Initialization
-    ###################################################################################
+        [self.root_window.grid_columnconfigure(index=i, weight=1) for i in range(9)]
 
-    # set the width and height of the original window
-    _w, _h = 800, 300
-    # main is the master name in which all work is going to be done
-    root_window = tk.Tk()
-    # set the title of the GUI window
-    root_window.title('SolarGUI')
-    # set the size of the GUI window
-    root_window.geometry(f'{_w}x{_h}')
+        # put main label inside the tkinter window
+        self.label = tk.Label(master=self.root_window,
+                              text='Welcome to Solar Explorer. Please select a button.')
+        self.label.grid(row=0, column=0, columnspan=9, pady=10,
+                        ipady=10)
 
-    # do not let the window be resized
-    root_window.minsize(_w, _h)
+        tk_f.object_button(window=self.root_window, text='Sun',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Sun',
+                                   object_class=Sun()), column=0)
 
-    # column width adjustment
-    [root_window.grid_columnconfigure(index=i, weight=1) for i in range(5)]
+        tk_f.object_button(window=self.root_window, text='Mercury',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Mercury',
+                                   object_class=Mercury),
+                           column=1)
 
-    ##################################################################################
-    # Working
-    ##################################################################################
+        tk_f.object_button(window=self.root_window, text='Venus',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Venus',
+                                   object_class=Venus),
+                           column=2)
 
-    # put main label inside the tkinter window
-    tk.Label(master=root_window,
-             text='Welcome to Solar Explorer. Please select a button.').grid(row=0,
-                                                                             column=0,
-                                                                             columnspan=9,
-                                                                             pady=10,
-                                                                             ipady=10)
+        tk_f.object_button(window=self.root_window, text='Earth',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Earth',
+                                   object_class=Earth),
+                           column=3)
 
-    # place the celestial body object buttons on the tkinter window
-    tk_f.object_button(window=root_window, text='Sun',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Sun',
-                                                                   object_class=Sun),
-                       column=0)
+        tk_f.object_button(window=self.root_window, text='Mars',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Mars',
+                                   object_class=Mars),
+                           column=4)
 
-    tk_f.object_button(window=root_window, text='Mercury',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Mercury',
-                                                                   object_class=Mercury),
-                       column=1)
+        tk_f.object_button(window=self.root_window, text='Jupiter',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Jupiter',
+                                   object_class=Jupiter),
+                           column=5)
 
-    tk_f.object_button(window=root_window, text='Venus',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Venus',
-                                                                   object_class=Venus),
-                       column=2)
+        tk_f.object_button(window=self.root_window, text='Saturn',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Saturn',
+                                   object_class=Saturn),
+                           column=6)
 
-    tk_f.object_button(window=root_window, text='Earth',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Earth',
-                                                                   object_class=Earth),
-                       column=3)
+        tk_f.object_button(window=self.root_window, text='Uranus',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Uranus',
+                                   object_class=Uranus),
+                           column=7)
 
-    tk_f.object_button(window=root_window, text='Mars',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Mars',
-                                                                   object_class=Mars),
-                       column=4)
+        tk_f.object_button(window=self.root_window, text='Neptune',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Neptune',
+                                   object_class=Neptune),
+                           column=8)
 
-    tk_f.object_button(window=root_window, text='Jupiter',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Jupiter',
-                                                                   object_class=Jupiter),
-                       column=5)
+        tk_f.object_button(window=self.root_window, text='Moon',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Moon',
+                                   object_class=Moon),
+                           row=2, column=0)
 
-    tk_f.object_button(window=root_window, text='Saturn',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Saturn',
-                                                                   object_class=Saturn),
-                       column=6)
+        tk_f.object_button(window=self.root_window, text='Pluto',
+                           function=lambda: sp.GetParameterSelection(
+                                   window=self.root_window,
+                                   title='Pluto',
+                                   object_class=Pluto),
+                           row=2, column=1)
 
-    tk_f.object_button(window=root_window, text='Uranus',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Uranus',
-                                                                   object_class=Uranus),
-                       column=7)
-
-    tk_f.object_button(window=root_window, text='Neptune',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Neptune',
-                                                                   object_class=Neptune),
-                       column=8)
-
-    tk_f.object_button(window=root_window, text='Moon',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Moon',
-                                                                   object_class=Moon),
-                       row=2, column=0)
-
-    tk_f.object_button(window=root_window, text='Pluto',
-                       function=lambda: sp.get_parameter_selection(window=root_window,
-                                                                   title='Pluto',
-                                                                   object_class=Pluto),
-                       row=2, column=1)
-
-    ##################################################################################
-    # Show it
-    ##################################################################################
-    root_window.mainloop()
+        self.root_window.mainloop()
 
 
 # taken from
@@ -150,6 +135,6 @@ def main():
 
 if __name__ == '__main__':
     try:
-        sys.exit(main())
+        sys.exit(Main())
     except TypeError:
         pass
